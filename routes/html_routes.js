133c8305918d085e08510f59fca1a25d6e666824
SofaKB <hall.b17@gmail.com>
2020-04-04 23:33:09 -0400

    mvc paradigm

---
+++ routes/html_routes.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+app.get('/', function(req, res){
+    res.render('index')
+});
+
+app.get('/portfolio', function(req, res){
+    res.render('portfolio')
+});
+
+app.get('/contact', function(req, res){
+    res.render('contact')
+});
+}
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const exphbs =  require("express-handlebars")
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const db = require("./models");
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use("/public", express.static(__dirname + "/public"))
+
+app.engine('handlebars', exphbs({ defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
+
+require("./routes/html_routes")(app);
+
+
+db.sequelize.sync({ force: true }).then(function(){
+    app.listen(PORT, function(){
+        console.log("Server listening on PORT " + PORT);
+    });
+});
